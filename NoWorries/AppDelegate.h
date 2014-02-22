c70843b5b2dc6517938a0d28337899ec3801f4d0
shanthi <shanthijayasekar@gmail.com>
2014-02-22 08:05:58 -0800

    Initial Commit

---
+++ NoWorries/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NoWorries
+//
+//  Created by user on 2/22/14.
+//  Copyright (c) 2014 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
